77107e36443ba0cc2843116cd9f84ba94a7e3701
confident <confidenthospital@gmail.com>
2017-07-13 19:44:37 +0530

    Booking Working

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,7 @@ module.exports = {
   secure: true,
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://manaswaoral:manaswaoral123@ds157712.mlab.com:57712/manaswaoral',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://manaswaoral:manaswaoral123@ds157712.mlab.com:57712/manaswaoraldb',
     options: {
       user: '',
       pass: ''

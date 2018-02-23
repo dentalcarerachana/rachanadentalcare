162c07a6add19b314606cb085d4a5e432cd816d4
shimmeringsmile <shimmeringsmilertwo@gmail.com>
2018-02-23 18:30:28 +0530

    changes

--- config/env/default.js
+++ config/env/default.js
@@ -28,7 +28,7 @@ module.exports = {
     sessionKey: 'sessionId',
     sessionCollection: 'sessions',
     logo: 'modules/core/img/brand/manaswaoral_logo.png',
-    favicon: 'modules/core/img/brand/favicon.ico',
+    favicon: 'modules/core/img/brand/mo.ico',
     uploads: {
         profileUpload: {
             dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
